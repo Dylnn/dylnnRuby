e397c0d6c39596349aba40ce474ff5bfb561e3ed
王明明 <IreHovel@oumatoMacBook-Pro.local>
2016-06-16 23:26:31 +0800

    tired of heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec rails server -p $PORT
+web: bundle exec ruby baidu_scholar_download.rb -p $PORT
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615152155) do
+ActiveRecord::Schema.define(version: 20140707111715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "widgets", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
